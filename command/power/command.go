bec0d1f974ac6e56a9e284e46fb986db5d96f4f2
Andrea Funtò <dihedron.dev@gmail.com>
2026-03-13 21:48:47 +0100

    Add timeout to shutdown and hibernate commands

--- command/power/command.go
+++ command/power/command.go
@@ -3,30 +3,42 @@ package power
 import (
 	"fmt"
 	"log/slog"
+	"time"
 
 	"github.com/dihedron/slumberd/command/base"
+	"github.com/dihedron/slumberd/timex"
 	"github.com/godbus/dbus/v5"
 )
 
 // Shutdown is the command that shuts down the machine.
 type Shutdown struct {
 	base.Command
+	// Timeout is the duration to wait before shutting down.
+	Timeout timex.Duration `short:"t" long:"timeout" description:"Timeout before shutting down." default:"0s"`
 }
 
 // Execute is the real implementation of the Shutdown command.
 func (cmd *Shutdown) Execute(args []string) error {
-	slog.Info("requesting system shutdown")
+	slog.Info("requesting system shutdown", "timeout", cmd.Timeout.String())
+	if cmd.Timeout > 0 {
+		time.Sleep(time.Duration(cmd.Timeout))
+	}
 	return callLogind("PowerOff")
 }
 
 // Hibernate is the command that hibernates the machine.
 type Hibernate struct {
 	base.Command
+	// Timeout is the duration to wait before hibernating.
+	Timeout timex.Duration `short:"t" long:"timeout" description:"Timeout before hibernating." default:"0s"`
 }
 
 // Execute is the real implementation of the Hibernate command.
 func (cmd *Hibernate) Execute(args []string) error {
-	slog.Info("requesting system hibernation")
+	slog.Info("requesting system hibernation", "timeout", cmd.Timeout.String())
+	if cmd.Timeout > 0 {
+		time.Sleep(time.Duration(cmd.Timeout))
+	}
 	return callLogind("Hibernate")
 }
 
